6fddd20674c082600a28b50b5a2e210cc7c15a66
halilozat <halilozat@hotmail.com>
2021-08-16 12:57:13 +0300

    Added new floor

--- src/App.js
+++ src/App.js
@@ -32,3 +32,35 @@ export default function App() {
   )
 }
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/header/Header';
+import Add from './pages/Add';
+import Home from './pages/Home';
+import "./index.css";
+
+function App() {
+  return (
+
+
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/add">
+          <Add />
+        </Route>
+        <Route />
+      </Switch>
+    </Router>
+
+
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { LinkLayer } from './context/LinkContext';
-import reducer, { initialState } from './context/reducer'
-
-
-
+import LinkContextProvider from './contexts/LinkContext';
+import { ToastContextProvider } from "./contexts/ToastContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <LinkLayer initialState={initialState} reducer={reducer}>
-      <App />
-    </LinkLayer>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ToastContextProvider>
+        <LinkContextProvider>
+            <App />
+        </LinkContextProvider>
+    </ToastContextProvider>,
+    document.getElementById("root")
 );
